0de6522b892ff03b1ee869d5fd38355bbee97ebd
onimaru-apex <68993404+onimaru-apex@users.noreply.github.com>
2021-01-02 07:22:37 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def team_norem(ctx, specified_num=2):
 async def group(ctx, specified_num=1):
     make_team = MakeTeam()
     deleted = await channel.purge(limit=100)
-await channel.send('Deleted {} message(s)'.format(len(deleted)))
+    await channel.send('Deleted {} message(s)'.format(len(deleted)))
     
     msg = make_team.make_specified_len(ctx,specified_num)
     await ctx.channel.send(msg)
